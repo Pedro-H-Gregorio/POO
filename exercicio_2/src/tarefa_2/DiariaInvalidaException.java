3c00dfb705369e0c3b6629807eba4af05eee8390
Pedro Gregório <pedrosgregorio@gmail.com>
2023-09-13 10:05:04 -0300

    Terminando reserva

---
+++ exercicio_2/src/tarefa_2/DiariaInvalidaException.java
@@ -0,0 +1,7 @@
+package tarefa_2;
+
+public class DiariaInvalidaException extends Exception{
+    public DiariaInvalidaException(){
+        super("Tempo de diária inválido.");
+    }
+}
--- exercicio_2/src/tarefa_2/Reserva.java
+++ exercicio_2/src/tarefa_2/Reserva.java
@@ -3,11 +3,13 @@ package tarefa_2;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Reserva {
     private int numeroQuarto;
     private String nomeHospede;
     final private double VALOR_DIARIA = 150.00;
+    private double valor_reserva;
     private Date checkIn;
     private Date checkOut;
 
@@ -22,6 +24,10 @@ public class Reserva {
         return nomeHospede;
     }
 
+    public double getValor_reserva() {
+        return valor_reserva;
+    }
+
     public Date getCheckIn() {
         return checkIn;
     }
@@ -30,24 +36,47 @@ public class Reserva {
         return checkOut;
     }
 
-    public boolean quartoIndisponivel(int numeroQuarto, Date checkIn, Date CheckOut){
-        return this.numeroQuarto == numeroQuarto &&
-                (checkIn.after(this.checkIn) && checkIn.before(this.checkOut)) &&
-                (CheckOut.after(this.checkIn) && CheckOut.before(this.checkOut));
+    public boolean quartoIndisponivel(){
+        return this.numeroQuarto != Integer.parseInt(null) && this.checkIn != null && this.checkOut != null;
     }
 
-    public void reservar(int numeroQuarto, String nomeHospede, String checkIn, String checkOut) throws ParseException, QuartoIndisponvelException {
+    public int tempoDaReserva(Date checkIn, Date checkOut) throws DiariaInvalidaException {
+        long diferencaDias = Math.abs(checkOut.getTime() - checkIn.getTime());
+        long tempo = TimeUnit.DAYS.convert(diferencaDias, TimeUnit.MILLISECONDS);
+        if(tempo < this.MIN_DIAS || tempo > this.MAX_DIAS){
+            throw new DiariaInvalidaException();
+        }
+        return (int) tempo;
+    }
+
+    public Reserva(int numeroQuarto, String nomeHospede, String checkIn, String checkOut) throws ParseException, QuartoIndisponvelException, DiariaInvalidaException {
+        // formatando datas
         SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
         Date newCheckIn = formato.parse(checkIn);
         Date newCheckOut = formato.parse(checkOut);
 
-        if(quartoIndisponivel(numeroQuarto, newCheckIn, newCheckOut)){
+        //verificando disponibilidade do quarto
+        if(quartoIndisponivel()){
             throw new QuartoIndisponvelException();
         }
+
+        // aplicando desconto a depender da quantidade de dias
+        int tempo = tempoDaReserva(newCheckIn, newCheckOut);
+        double desconto = tempo > 3 ? 0 : 0.10;
+
+        this.valor_reserva = (VALOR_DIARIA * desconto - VALOR_DIARIA) * tempo;
         this.nomeHospede = nomeHospede;
         this.numeroQuarto = numeroQuarto;
         this.checkIn = newCheckIn;
         this.checkOut = newCheckOut;
+    }
+
+    public void cancelarReserva(){
+        this.numeroQuarto = Integer.parseInt(null);
+        this.nomeHospede = null;
+        this.checkIn = null;
+        this.checkOut = null;
+        this.valor_reserva = 0;
     }
 
 }
